b504bdb1938471935e67368ed5a6795e2c3f491b
Manish <my45516gy@gmail.com>
2026-08-31 02:48:02 +0530

    Update project

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-print("How to push code in github.")
+# print("How to push code in github.")
+import subprocess
+
+while True:
+    text = input("You: ")
+
+    if text.lower() == "exit":
+        break
+
+    if not text.strip():
+        continue
+
+    subprocess.run([
+        "python",
+        "-m",
+        "piper",
+        "-m",
+        "en_US-lessac-medium",
+        "--output_file",
+        "speech.wav"
+    ], input=text.encode())
+
+    # Play the generated WAV file
+    subprocess.run([
+        "powershell",
+        "-c",
+        "(New-Object Media.SoundPlayer 'speech.wav').PlaySync()"
+    ])
